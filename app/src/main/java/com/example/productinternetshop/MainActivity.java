b0a80caa3d01c212c117fa1b1f178ffcb580310f
darya-cherr <chernikovadarya67@gmail.com>
2022-03-29 04:05:03 +0300

    Added bottom navigation bar

--- app/src/main/java/com/example/productinternetshop/MainActivity.java
+++ app/src/main/java/com/example/productinternetshop/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.productinternetshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
 public class MainActivity extends AppCompatActivity {
+
+    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navbar);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_icons8_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_search_svgrepo_com));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_heart_svgrepo_com));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.ic_user_profile_svgrepo_com));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+                switch (item.getId()){
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+                    case 2:
+                        fragment = new ExploreFragment();
+                        break;
+                    case 3:
+                        fragment = new FavouriteFragment();
+                        break;
+                    case 4:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+
+        bottomNavigation.show(1,true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+
     }
 }
